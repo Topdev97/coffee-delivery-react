c8cfdc86f0bd1f699bfc720aff0ceccf763555eb
Artem0328 <laoroney1113@gmail.com>
2023-08-29 18:48:48 -0300

    Create header + add typed default theme + implement fonts

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
+import { ThemeProvider } from 'styled-components'
+
+import { Header } from './components/Header'
+
 import { GlobalStyle } from './styles/global'
+import { defaultTheme } from './styles/themes/default'
 
 export function App() {
   return (
-    <>
-      <h1>Coffee Delivery App</h1>
+    <ThemeProvider theme={defaultTheme}>
+      <Header />
       <GlobalStyle />
-    </>
+    </ThemeProvider>
   )
 }
---
+++ src/components/Header/styles.ts
@@ -0,0 +1,62 @@
+import { styled } from 'styled-components'
+
+export const HeaderContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  padding: 2rem 10rem;
+
+  nav {
+    display: flex;
+    gap: 0.75rem;
+  }
+`
+
+const NavigationBase = styled.div`
+  display: flex;
+  align-items: center;
+
+  gap: 0.25rem;
+  padding: 0.5rem;
+  border-radius: 6px;
+
+  span {
+    text-transform: uppercase;
+  }
+`
+
+export const LocationContainer = styled(NavigationBase)`
+  color: ${(props) => props.theme['purple-dark']};
+  background-color: ${(props) => props.theme['purple-light']};
+`
+
+export const CartContainer = styled(NavigationBase)`
+  position: relative;
+
+  color: ${(props) => props.theme['yellow-dark']};
+  background-color: ${(props) => props.theme['yellow-light']};
+`
+
+export const CartNotification = styled.div`
+  position: absolute;
+  right: -8px;
+  top: -8px;
+
+  width: 1.25rem;
+  height: 1.25rem;
+
+  border-radius: 50%;
+
+  font-family: 'Baloo 2', cursive;
+  font-size: 12px;
+
+  color: ${(props) => props.theme.white};
+  background-color: ${(props) => props.theme['yellow-dark']};
+
+  span {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+`
