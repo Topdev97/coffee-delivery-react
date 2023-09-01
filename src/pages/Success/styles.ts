e88d48efb49683731889d4bf135a3ff4b9ea532e
Artem0328 <laoroney1113@gmail.com>
2023-08-31 21:04:13 -0300

    Make success screen mobile-responsive

--- src/pages/Success/styles.ts
+++ src/pages/Success/styles.ts
@@ -2,8 +2,14 @@ import styled from 'styled-components'
 
 export const SuccessContainer = styled.div`
   display: flex;
+  flex-wrap: wrap;
   flex-direction: column;
   padding: 5rem 10rem 0;
+
+  @media (max-width: 768px) {
+    width: 100%;
+    padding: 2rem;
+  }
 `
 
 export const TitleContainer = styled.div`
@@ -26,10 +32,20 @@ export const Message = styled.span`
 
 export const ContentContainer = styled.div`
   display: flex;
+  flex-wrap: wrap;
   justify-content: space-between;
 
   img {
     width: 30.75rem;
+  }
+
+  @media (max-width: 768px) {
+    width: 100%;
+
+    img {
+      width: 100%;
+      margin-top: 2rem;
+    }
   }
 `
 
@@ -37,8 +53,8 @@ export const OrderInfoContainer = styled.div`
   width: 32.875rem;
   padding: 2.5rem;
 
-  border-radius: 6px 36px; /*1*/
-  border: 1px solid transparent; /*2*/
+  border-radius: 6px 36px;
+  border: 1px solid transparent;
   background: linear-gradient(
       135deg,
       ${(props) => props.theme.yellow},
@@ -47,12 +63,12 @@ export const OrderInfoContainer = styled.div`
     border-box;
 
   mask:
-    linear-gradient(#fff 0 0) padding-box,
-    linear-gradient(#fff 0 0);
+    linear-gradient(${(props) => props.theme.background} 0 0) padding-box,
+    linear-gradient(${(props) => props.theme.background} 0 0);
 
   -webkit-mask:
-    linear-gradient(#fff 0 0) padding-box,
-    linear-gradient(#fff 0 0);
+    linear-gradient(${(props) => props.theme.background} 0 0) padding-box,
+    linear-gradient(${(props) => props.theme.background} 0 0);
   -webkit-mask-composite: xor;
   mask-composite: exclude;
 `
